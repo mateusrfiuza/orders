05311d58febbf20f69ce870bda711a6a09f1bb27
Mateus Fiuza <mateus130@hotmail.com>
2024-01-18 11:12:45 -0300

    Changed system out for logger in CreateInvoiceUseCase

--- src/main/java/com/sample/domain/logistics/usecase/CreateInvoiceUseCase.java
+++ src/main/java/com/sample/domain/logistics/usecase/CreateInvoiceUseCase.java
@@ -1,12 +1,16 @@
 package com.sample.domain.logistics.usecase;
 
+import com.sample.domain.membership.dataprovider.event.MembershipNotifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateInvoiceUseCase {
 
+    private static final Logger logger = LoggerFactory.getLogger(CreateInvoiceUseCase.class);
 
     public void execute() {
-        System.out.println("Creating invoice");
+        logger.info("Creating invoice");
     }
 }
