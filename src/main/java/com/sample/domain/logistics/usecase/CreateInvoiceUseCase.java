1280f73e303cc2b5ccb91814b5ead412ee921a8d
Mateus Fiuza <mateus130@hotmail.com>
2024-01-18 11:13:38 -0300

    removing unused log for CreateInvoiceUseCase

--- src/main/java/com/sample/domain/logistics/usecase/CreateInvoiceUseCase.java
+++ src/main/java/com/sample/domain/logistics/usecase/CreateInvoiceUseCase.java
@@ -1,6 +1,5 @@
 package com.sample.domain.logistics.usecase;
 
-import com.sample.domain.membership.dataprovider.event.MembershipNotifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
