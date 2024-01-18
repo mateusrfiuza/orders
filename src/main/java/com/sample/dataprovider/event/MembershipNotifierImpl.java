76659aa0c7132168cc3342ee87d87f76da3223aa
Mateus Fiuza <mateus130@hotmail.com>
2024-01-17 22:29:12 -0300

    added missing annotation and removed blank spaces

--- src/main/java/com/sample/dataprovider/event/MembershipNotifierImpl.java
+++ src/main/java/com/sample/dataprovider/event/MembershipNotifierImpl.java
@@ -1,7 +1,9 @@
 package com.sample.dataprovider.event;
 
 import com.sample.domain.membership.dataprovider.event.MembershipNotifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MembershipNotifierImpl implements MembershipNotifier {
     @Override
     public void notifyCustomer() {
--- src/main/java/com/sample/domain/logistics/usecase/CreateInvoiceUseCase.java
+++ src/main/java/com/sample/domain/logistics/usecase/CreateInvoiceUseCase.java
@@ -6,10 +6,6 @@ import org.springframework.stereotype.Service;
 public class CreateInvoiceUseCase {
 
 
-
-
-
-
     public void execute() {
         System.out.println("Creating invoice");
     }
