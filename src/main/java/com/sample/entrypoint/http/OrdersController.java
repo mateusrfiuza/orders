d33dbf5b25891cc131f391d47838b0469752229a
Mateus Fiuza <mateus130@hotmail.com>
2024-01-17 22:56:59 -0300

    removed unused dependencies

--- src/main/java/com/sample/entrypoint/http/OrdersController.java
+++ src/main/java/com/sample/entrypoint/http/OrdersController.java
@@ -3,16 +3,15 @@ package com.sample.entrypoint.http;
 import com.sample.domain.orders.usecase.CreateOrderUseCase;
 import com.sample.entrypoint.http.payload.CreationOrderRequest;
 import com.sample.entrypoint.http.payload.CreationOrderResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Validated
