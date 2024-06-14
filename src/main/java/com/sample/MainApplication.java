1d319c3f20e1623a891a7ff04ff618c98f29e4be
Mateus Fiuza <mateus130@hotmail.com>
2024-06-14 12:37:23 -0300

    Removed unused dependency

--- src/main/java/com/sample/MainApplication.java
+++ src/main/java/com/sample/MainApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableConfigurationProperties
